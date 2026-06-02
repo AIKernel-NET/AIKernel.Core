c871ad9dc55224f7b5edbe2efb95fe85d5bd5c71
TKY S <tky4@hotmail.com>
2026-06-02 22:36:01 +0900

    chore: remove local abstractions staging

--- src/AIKernel.Core/Abstractions/PromptProjectionOptions.cs
+++
@@ -1,6 +0,0 @@
-#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
-namespace AIKernel.Dtos.Execution;
-#pragma warning restore IDE0130 // Namespace がフォルダー構造と一致しません
-
-public sealed record PromptProjectionOptions(
-    bool IncludeSourceMetadata);
--- src/AIKernel.Core/Abstractions/RomSignatureSnapshot.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
-namespace AIKernel.Dtos.Rom;
-#pragma warning restore IDE0130 // Namespace がフォルダー構造と一致しません
-
-public sealed record RomSignatureSnapshot(
-    string Algorithm,
-    string ExpectedHash,
-    string ActualHash,
-    bool IsVerified);
